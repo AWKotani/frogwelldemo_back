6ad777c9d277c1177a5d25940f84577911aaf3a4
EC2 Default User <ec2-user@ip-172-31-34-126.ap-northeast-1.compute.internal>
2023-03-22 14:56:47 +0000

    LINE login version4

--- src/App.js
+++ src/App.js
@@ -14,9 +14,10 @@ function App() {
   /*******EVENT****** */
 
   useEffect(() => {
+      liff.ready.then(() => {
     if (liff.isLoggedIn()) {
       console.log(liff.isLoggedIn())
-    }
+    }})
   }, [])
 
   if (liff.isLoggedIn()){
