c8dbda22f9dbea5c1be729ace4a8ae8c1a52892c
EC2 Default User <ec2-user@ip-172-31-34-126.ap-northeast-1.compute.internal>
2023-03-28 17:02:37 +0000

    Source code clean-up

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import 'antd/dist/reset.css';
 import liff from '@line/liff/dist/lib';
-//import jsforce from 'jsforce';
+import jsforce from 'jsforce';
 
 export const Mainpage = (() => {
   const [Userprofile , setUserprofile] = useState('');
@@ -10,23 +10,15 @@ export const Mainpage = (() => {
     console.log(user);
     setUserprofile(user);
   });
-  return (
-    <div>
-      <button onClick={Getuser}>Show User info.</button>
-      <p>userId:{Userprofile.userId}</p>
-      <p>displayName:{Userprofile.displayName}</p>
-    </div>
-  );
-/*  const recs = '';
-//  (async () => {
-    const conn = new jsforce.Connection({
-      oauth2 : {
-        loginUrl: 'https://test.salesforce.com',
-        clinentId: '3MVG9aWdXtdHRrI33PiDvXP2NYIQhsXP0XFxoKwRvQZaCA8YO.AmL2bY9h6xMnzcKCbANo3tbvpX.4lMMwu63',
-        clientSecret: 'A523BAFA65382BC2AD83D766DBA032053D0CEC077C8223DD92F945A029D6FF3C',
-        redirectUri: 'https://frogwell--techskill.sandbox.my.salesforce.com/oauth2/callback&response_type=code'
-      }
-    });
+  
+  const conn = new jsforce.Connection({
+    oauth2 : {
+      loginUrl: 'https://test.salesforce.com',
+      clinentId: '3MVG9aWdXtdHRrI33PiDvXP2NYIQhsXP0XFxoKwRvQZaCA8YO.AmL2bY9h6xMnzcKCbANo3tbvpX.4lMMwu63',
+      clientSecret: 'A523BAFA65382BC2AD83D766DBA032053D0CEC077C8223DD92F945A029D6FF3C',
+      redirectUri: 'https://frogwell--techskill.sandbox.my.salesforce.com/oauth2/callback&response_type=code'
+    }
+  });
 //    await conn.login('api.user@frogwell.co.jp.techskill', 'frogwell20236h9WlVSOU9UDbhl5lt0CcBv3', function(err, userInfo) {
     conn.login('api.user@frogwell.co.jp.techskill', 'frogwell20236h9WlVSOU9UDbhl5lt0CcBv3', function(err, userInfo) {
       if (err) { return console.error(err); }
@@ -36,5 +28,12 @@ export const Mainpage = (() => {
       LineUserId__c: 'U75bcd602fbd7da18d3974ac788bc7f00'
     });
 //  });
-*/
+  return (
+    <div>
+      <button onClick={Getuser}>Show User info.</button>
+      <p>userId:{Userprofile.userId}</p>
+      <p>displayName:{Userprofile.displayName}</p>
+      <p>salesforceId:{recs.Id}</p>
+    </div>
+  );
 });
